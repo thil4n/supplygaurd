5ad33110e91d89e64cd3ebfd97cff14dc43451b1
Thilan Dissanayaka <thil4n@gmail.com>
2026-01-13 14:45:24 +0530

    Reading the package.json content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod package_json;
+
+use crate::package_json::PackageJson;
+
+use std::fs;
+use serde_json;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = "datasets/benign/package.json";
+
+    let content = fs::read_to_string(file_path)?;
+    println!("Package.json read successfully\n");
+
+    let pkg: PackageJson = serde_json::from_str(&content)?;
+    println!("Parsed PackageJson: {:?}", pkg);
+
+    Ok(())
 }
---
+++ src/package_json.rs
@@ -0,0 +1,11 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+pub struct PackageJson {
+    pub name: Option<String>,
+    pub version: Option<String>,
+    pub scripts: Option<HashMap<String, String>>,
+    pub dependencies: Option<HashMap<String, String>>,
+    pub devDependencies: Option<HashMap<String, String>>,
+}
